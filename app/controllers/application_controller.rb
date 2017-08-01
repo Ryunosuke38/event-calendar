9f3ed5d709fc345df481a5dba29329342e9ef3b2
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-08-02 06:24:48 +0900

    fix date-display in meetings/index.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :set_time_zone, if: :user_signed_in?
+  # before_filter :set_time_zone, if: :user_signed_in?
 
-  private
+  # private
 
-    def set_time_zone
-      Time.zone = current_user.time_zone
-    end
+  #   def set_time_zone
+  #     Time.zone = current_user.time_zone
+  #   end
 end
